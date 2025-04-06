fce819e11c35f96641d6a70578a984efcd95302a
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-06 16:56:46 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <header
+      className="bg-lime-900 text-white p-4 flex justify-center items-center"
+      >
+        <h1 className="container mx-auto text-2xl font-bold text-center">
+          Calculadora de propinas y consumo
+          </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
